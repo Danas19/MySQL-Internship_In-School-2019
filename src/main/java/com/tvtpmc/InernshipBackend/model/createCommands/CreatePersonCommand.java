cc662c9d749b709322adfea6c6281b89e2dfbb37
Danas <danas.mikelionis@gmail.com>
2019-06-06 09:12:47 +0300

    modified CreatePersonCommand, left only 2 variables and added getters and setters

--- src/main/java/com/tvtpmc/InernshipBackend/model/createCommands/CreatePersonCommand.java
+++ src/main/java/com/tvtpmc/InernshipBackend/model/createCommands/CreatePersonCommand.java
@@ -1,18 +1,24 @@
 package com.tvtpmc.InernshipBackend.model.createCommands;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.tvtpmc.InernshipBackend.model.Document;
-import com.tvtpmc.InernshipBackend.model.User;
-import com.tvtpmc.InernshipBackend.model.UserGroup;
-
 public class CreatePersonCommand {
-	private Set<Document> myDocuments = new HashSet<Document>();
-	private Set<Document> acceptedDocuments = new HashSet<Document>();
 	private String firstName;
 	private String lastName;
-	private Long adminId;
-	private User userId;
-	private UserGroup userGroup;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+		
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 }
