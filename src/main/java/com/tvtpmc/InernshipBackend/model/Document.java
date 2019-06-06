f3a4532e072b47d77222af26db40c835cfb00fc1
Danas <danas.mikelionis@gmail.com>
2019-06-06 09:49:40 +0300

    modified Document.java. Added getters, setters

--- src/main/java/com/tvtpmc/InernshipBackend/model/Document.java
+++ src/main/java/com/tvtpmc/InernshipBackend/model/Document.java
@@ -46,4 +46,78 @@ public class Document {
 	private String declinationReason;
 	@Lob
 	private Blob file;
+	
+	public Person getAuthor() {
+		return author;
+	}
+	
+	public DocumentType getDocumentType() {
+		return documentType;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Date getAcceptanceDate() {
+		return acceptanceDate;
+	}
+	
+	public Date getDeclinationDate() {
+		return declinationDate;
+	}
+	
+	public Person getAccepter() {
+		return accepter;
+	}
+	
+	public String getDeclinationReason() {
+		return declinationReason;
+	}
+	
+	public Blob getFile() {
+		return file;
+	}
+	
+	public void setAuthor(Person author) {
+		this.author = author;
+	}
+	
+	public void setDocumentType(DocumentType documentType) {
+		this.documentType = documentType;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setAcceptanceDate(Date acceptanceDate) {
+		this.acceptanceDate = acceptanceDate;
+	}
+	
+	public void setDeclinationDate(Date declinationDate) {
+		this.declinationDate = declinationDate;
+	}
+	
+	public void setAccepter(Person accepter) {
+		this.accepter = accepter;
+	}
+	
+	public void setDeclinationReason(String declinationReason) {
+		this.declinationReason = declinationReason;
+	}
+	
+	public void setFile(Blob file) {
+		this.file = file;
+	}
+	
+	
 }
