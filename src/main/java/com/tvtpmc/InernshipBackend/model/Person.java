1793af612e24d3253d0622c95ba6c6b6ade3449b
Danas <danas.mikelionis@gmail.com>
2019-06-06 09:29:18 +0300

    modified Person.java. Added getters, setters, addMyDocument() and addMyAcceptedDocument() methods. Changed acceptedDocuments variable to myAcceptedDocuments

--- src/main/java/com/tvtpmc/InernshipBackend/model/Person.java
+++ src/main/java/com/tvtpmc/InernshipBackend/model/Person.java
@@ -19,7 +19,7 @@ public class Person {
 	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
 	private Set<Document> myDocuments;
 	@OneToMany(mappedBy = "accepter", cascade = CascadeType.ALL)
-	private Set<Document> acceptedDocuments;
+	private Set<Document> myAcceptedDocuments;
 	private String firstName;
 	private String lastName;
 	@OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
@@ -29,8 +29,62 @@ public class Person {
 	
 	public Person() {
 		myDocuments = new HashSet<Document>();
-		acceptedDocuments = new HashSet<Document>();
+		myAcceptedDocuments = new HashSet<Document>();
+	}
+
+	public Set<Document> getMyDocuments() {
+		return myDocuments;
+	}
+
+	public Set<Document> getMyAcceptedDocuments() {
+		return myAcceptedDocuments;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Admin getAdmin() {
+		return admin;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setMyDocuments(Set<Document> myDocuments) {
+		this.myDocuments = myDocuments;
+	}
+
+	public void setMyAcceptedDocuments(Set<Document> myAcceptedDocuments) {
+		this.myAcceptedDocuments = myAcceptedDocuments;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
+	public void addMyDocument(Document document) {
+		myDocuments.add(document);
+	}
 	
+	public void addMyAcceptedDocuments(Document document) {
+		this.myAcceptedDocuments.add(document);
+	}
 }
