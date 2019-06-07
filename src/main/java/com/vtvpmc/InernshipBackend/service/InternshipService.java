2a23b4aed08b1405a62013c4274f11f6e7cc5f9a
Danas <danas.mikelionis@gmail.com>
2019-06-07 12:05:53 +0300

    removed in Service unused imports

--- src/main/java/com/vtvpmc/InernshipBackend/service/InternshipService.java
+++ src/main/java/com/vtvpmc/InernshipBackend/service/InternshipService.java
@@ -4,10 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.vtvpmc.InernshipBackend.model.Admin;
 import com.vtvpmc.InernshipBackend.model.Document;
